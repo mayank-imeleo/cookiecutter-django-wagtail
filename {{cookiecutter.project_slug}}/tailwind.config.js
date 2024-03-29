a93f8359d01575b1fc4a467685d8caa8d724bcf9
Mayank Mahajan <mayankm2089@gmail.com>
2024-03-29 18:43:31 +0530

    fix: tailwind.config.js

--- {{cookiecutter.project_slug}}/tailwind.config.js
+++ {{cookiecutter.project_slug}}/tailwind.config.js
@@ -3,14 +3,23 @@ const colors = require("tailwindcss/colors");
 
 
 module.exports = {
-  content: ["{{cookiecutter.project_slug}}/**/*.{html,js}"],
-  theme: {
-    colors: {
-      primary: "{{cookiecutter.primary_color}}",
-      secondary: colors.gray,
-    }
-  },
-  plugins: []
+    content: ["imeleo/**/*.{html,js}"],
+    theme: {
+        colors: {
+            primary: colors.blue,
+            secondary: colors.gray,
+            white: colors.white,
+            gray: colors.gray,
+            transparent: 'transparent',
+            current: 'currentColor',
+            black: colors.black,
+            emerald: colors.emerald,
+            indigo: colors.indigo,
+            yellow: colors.yellow,
+            neutral: colors.neutral,
+        }
+    },
+    plugins: []
 };
 
 
